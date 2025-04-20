639546d09eba58897b17bad08458755c19462af8
MissMurph <murphyhmorgan@gmail.com>
2025-04-20 21:58:08 +1000

    More Harvest Work
    Getting confused...

--- Assets/Scripts/Ratworx/MarsTS/Commands/Receivers/DepositReceiver.cs
+++ Assets/Scripts/Ratworx/MarsTS/Commands/Receivers/DepositReceiver.cs
@@ -1,7 +1,50 @@
+using Ratworx.MarsTS.Commands.Commandlets;
+using Ratworx.MarsTS.Entities;
+using Ratworx.MarsTS.Events;
+using Ratworx.MarsTS.Events.Commands;
+using Ratworx.MarsTS.Events.Harvesting;
+using Ratworx.MarsTS.Events.Selectable.Attackable;
+using Ratworx.MarsTS.Units;
+using Ratworx.MarsTS.Units.Sensors;
+using UnityEngine;
+
 namespace Ratworx.MarsTS.Commands.Receivers
 {
-    public class DepositReceiver
+    public class DepositReceiver : MonoBehaviour,
+                                   IEntityUpdate
     {
+        [SerializeField] private DepositSensor _depositRange;
+        [SerializeField] private ResourceStorage _storage;
         
+        private DepositableCommandlet _depositCommand;
+
+        private UnitPathfinder _unitPathing;
+        private UnitTargetManager _unitTargeting;
+        private CommandQueue _commandQueue;
+        private EventAgent _eventAgent;
+
+        private void Awake() {
+            _eventAgent = GetComponent<EventAgent>();
+            _commandQueue = GetComponent<CommandQueue>();
+            _unitPathing = GetComponent<UnitPathfinder>();
+            _unitTargeting = GetComponent<UnitTargetManager>();
+        }
+        
+        private void Start() {
+            _eventAgent.AddListener<CommandStartEvent>(ReceiveCommand);
+        }
+        
+        private void ReceiveCommand(CommandStartEvent evnt) {
+            if (evnt.Command is not DepositableCommandlet deserialized
+                || evnt.Command.Name != "harvest") return;
+            
+            _depositCommand = deserialized;
+            _unitTargeting.SetTarget(_depositCommand.Target);
+
+            _depositCommand.Target.Entity.TryGetEntityComponent(out EventAgent targetBus);
+            targetBus.AddListener<HarvesterDepositEvent>(OnResourceDeposited);
+            targetBus.AddListener<UnitDeathEvent>(OnTargetDeath);
+            _depositCommand.Callback.AddListener(OnC);
+        }
     }
 }
--- Assets/Scripts/Ratworx/MarsTS/Commands/Receivers/HarvestReceiver.cs
+++ Assets/Scripts/Ratworx/MarsTS/Commands/Receivers/HarvestReceiver.cs
@@ -1,3 +1,4 @@
+using Ratworx.MarsTS.Buildings;
 using Ratworx.MarsTS.Commands.Commandlets;
 using Ratworx.MarsTS.Entities;
 using Ratworx.MarsTS.Events;
@@ -8,7 +9,6 @@ using Ratworx.MarsTS.Teams;
 using Ratworx.MarsTS.Units;
 using Ratworx.MarsTS.Units.Sensors;
 using Ratworx.MarsTS.WorldObject;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 namespace Ratworx.MarsTS.Commands.Receivers
@@ -17,13 +17,17 @@ namespace Ratworx.MarsTS.Commands.Receivers
                                    IEntityUpdate
     {
         [SerializeField] private HarvestSensor _harvestRange;
+        [SerializeField] private DepositSensor _depositRange;
+        [SerializeField] private ResourceStorage _storage;
+        [SerializeField] private bool _automaticallyReturn;
 
+        private HarvestableCommandlet _harvestCommand;
+        private IDepositable _depositable;
+        
         private UnitPathfinder _unitPathing;
         private UnitTargetManager _unitTargeting;
         private CommandQueue _commandQueue;
         private EventAgent _eventAgent;
-        
-        private HarvestableCommandlet _harvestCommand;
 
         private void Awake() {
             _eventAgent = GetComponent<EventAgent>();
@@ -38,53 +42,67 @@ namespace Ratworx.MarsTS.Commands.Receivers
 
         private void ReceiveCommand(CommandStartEvent evnt) {
             if (evnt.Command is not HarvestableCommandlet deserialized
-                || evnt.Command.Name != "harvest") 
-                return;
-			
-            IHarvestable unit = deserialized.Target;
-			
+                || evnt.Command.Name != "harvest") return;
+            
             _harvestCommand = deserialized;
             _unitTargeting.SetTarget(_harvestCommand.Target);
 			
             _harvestCommand.Target.Entity.TryGetEntityComponent(out EventAgent targetBus);
-            targetBus.AddListener<UnitHurtEvent>(OnTargetHealed);
-            targetBus.AddListener<UnitDeathEvent>(OnTargetDeath);
+            targetBus.AddListener<ResourceHarvestedEvent>(OnResourceHarvested);
+            targetBus.AddListener<UnitDeathEvent>(OnDepositDepleted);
             _harvestCommand.Callback.AddListener(OnCommandComplete);
         }
-        
+
         public void UpdateServer() {
+            if (_depositable is not null) 
+                UpdateDepositTarget();
+            else 
+                UpdateHarvestTarget();
+        }
+
+        private void UpdateHarvestTarget() {
             if (_harvestCommand is null) 
                 return;
-			
+
             if (_harvestRange.IsDetected(_harvestCommand.Target)) {
+                // Do we really want to be clearing the target every single frame?
                 _unitTargeting.ClearTarget();
                 _unitPathing.ClearPath();
             }
             else
-                _unitTargeting.SetTarget(_repairCommand.Target);
+                _unitTargeting.SetTarget(_harvestCommand.Target);
+        }
+
+        private void UpdateDepositTarget() {
+            if (_depositRange.IsDetected(_depositable)) {
+                _unitTargeting.ClearTarget();
+                _unitPathing.ClearPath();
+            }
+            else
+                _unitTargeting.SetTarget(_depositable);
         }
 
         public void UpdateClient() { }
         
         private void Harvest(Commandlet order)
         {
-            if (Stored >= Port.Capacity) FindDepositable();
+            if (Stored >= Capacity) FindDepositable();
 
             if (order is Commandlet<IHarvestable> deserialized)
             {
                 HarvestTarget = deserialized.Target;
 
-                Bus.AddListener<ResourceHarvestedEvent>(OnExtraction);
+                Bus.AddListener<ResourceHarvestedEvent>(OnResourceHarvested);
 
                 EntityCache.TryGetEntityComponent(HarvestTarget.GameObject.transform.root.name, out EventAgent targetBus);
 
                 targetBus.AddListener<UnitDeathEvent>(OnDepositDepleted);
 
-                order.Callback.AddListener(HarvestCancelled);
+                order.Callback.AddListener(OnCommandComplete);
             }
         }
         
-        private void OnExtraction(ResourceHarvestedEvent _event)
+        private void OnResourceHarvested(ResourceHarvestedEvent _event)
         {
             if (Stored >= Port.Capacity)
                 //bus.RemoveListener<ResourceHarvestedEvent>(OnExtraction);
@@ -97,18 +115,18 @@ namespace Ratworx.MarsTS.Commands.Receivers
 
         private void OnDepositDepleted(UnitDeathEvent _event)
         {
-            Bus.RemoveListener<ResourceHarvestedEvent>(OnExtraction);
+            Bus.RemoveListener<ResourceHarvestedEvent>(OnResourceHarvested);
 
             CommandCompleteEvent newEvent = new CommandCompleteEvent(Bus, CurrentCommand, false, this);
 
             CurrentCommand.Callback.Invoke(newEvent);
         }
 
-        private void HarvestCancelled(CommandCompleteEvent _event)
+        private void OnCommandComplete(CommandCompleteEvent _event)
         {
             if (_event.Command is Commandlet<IHarvestable> deserialized && _event.IsCancelled)
             {
-                Bus.RemoveListener<ResourceHarvestedEvent>(OnExtraction);
+                Bus.RemoveListener<ResourceHarvestedEvent>(OnResourceHarvested);
 
                 EntityCache.TryGetEntityComponent(deserialized.Target.GameObject.transform.root.name, out EventAgent targetBus);
 
